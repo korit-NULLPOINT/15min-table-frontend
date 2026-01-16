a24d52e48e4217811997a3f7503119b22ea013be
S-JaeWon <ssm5170@gmail.com>
2026-01-16 15:37:19 +0900

    Feat: 홈페이지 / 커뮤니티 / 레시피 분리

--- src/pages/boards/free/free-detail-page/FreeDetailPage.jsx
+++ src/pages/boards/free/free-detail-page/FreeDetailPage.jsx
@@ -1,6 +1,17 @@
-function FreeDetailPage() {
-  return (
-    <div>FreeDetailPage</div>
-  )
+import { useNavigate, useParams } from "react-router-dom";
+import { CommunityDetail } from "../../../../components/CommunityDetail";
+
+export default function FreeDetailPage() {
+    const { boardId, freeId } = useParams();
+    const navigate = useNavigate();
+
+    const onNavigate = (key) => {
+        if (key === "community") navigate(`/boards/${boardId}/free`);
+    };
+
+    return (
+        <>
+            <CommunityDetail postId={freeId} onNavigate={onNavigate} />
+        </>
+    );
 }
-export default FreeDetailPage
--- src/pages/boards/free/free-edit-page/FreeEditPage.jsx
+++ src/pages/boards/free/free-edit-page/FreeEditPage.jsx
@@ -1,6 +1,14 @@
-function FreeEditPage() {
-  return (
-    <div>FreeEditPage</div>
-  )
+import { useNavigate, useParams } from "react-router-dom";
+import { CommunityEdit } from "../../../../components/CommunityEdit";
+
+export default function FreeEditPage() {
+    const { boardId, freeId } = useParams();
+    const navigate = useNavigate();
+
+    const onNavigate = (key) => {
+        if (key === "community") navigate(`/boards/${boardId}/free`);
+        if (key === "detail") navigate(`/boards/${boardId}/free/${freeId}`);
+    };
+
+    return <CommunityEdit postId={freeId} onNavigate={onNavigate} />;
 }
-export default FreeEditPage
