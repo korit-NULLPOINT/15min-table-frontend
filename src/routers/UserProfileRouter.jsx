cb6e011206f1833489364d31d41bd73cce7c1070
S-JaeWon <ssm5170@gmail.com>
2026-01-23 09:05:36 +0900

    Feat: user-profile-route

--- src/routers/UserProfileRouter.jsx
+++ src/routers/UserProfileRouter.jsx
@@ -5,16 +5,11 @@ import FollowListPage from '../pages/me/follow-list-page/FollowListPage';
 export default function UserProfileRouter() {
     return (
         <Routes>
-            {/* /users/:userId */}
             <Route path=":userId" element={<OtherUserProfilePage />} />
-
-            {/* /users/:userId/followers */}
             <Route
                 path=":userId/followers"
                 element={<FollowListPage type="followers" />}
             />
-
-            {/* /users/:userId/following */}
             <Route
                 path=":userId/following"
                 element={<FollowListPage type="following" />}
